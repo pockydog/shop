16658b3f918caead04d2da5590a382b481537d61
vickychen <vickychen@fillygaming888.com>
2022-06-27 15:19:00 +0800

    Add product

---
+++ src/test/level_test.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class Test:
+    url = 'http://127.0.0.1:5000/level'
+    @classmethod
+    def add_info(cls):
+        url = cls.url
+        response = requests.post(url=url)
+        print(response.text)
+
+    @classmethod
+    def del_info(cls):
+        url = 'http://127.0.0.1:5000/level'
+        response = requests.delete(url=url, params={'level_id': 3})
+        print(response.text)
+
+    @classmethod
+    def get_info(cls):
+        url = 'http://127.0.0.1:5000/level'
+        response = requests.get(url=url)
+        print(response.text)
+
+    @classmethod
+    def update_info(cls):
+        url = 'http://127.0.0.1:5000/level'
+        response = requests.update(url=url)
+        print(response.text)
---
+++ src/test/user_test.py
@@ -0,0 +1,36 @@
+import requests
+from faker import Faker
+fake = Faker()
+
+
+class Test:
+    _URL = 'http://127.0.0.1:5000/user'
+    @classmethod
+    def get_info(cls):
+        response = requests.get(url=cls._URL)
+        print(response.text)
+
+    @classmethod
+    def add_info(cls):
+        payload = {
+            'name': f'{fake.first_name()}',
+            'account': f'{fake.unix_time()}',
+            'password': f'{fake.unix_time()}',
+            'phone_number': f'{fake.phone_number()}',
+        }
+        response = requests.post(url=cls._URL, json=payload)
+        print(response.text)
+
+    @classmethod
+    def del_info(cls):
+        response = requests.delete(url=cls._URL, params={'name': 'Holly', 'phone_number': '5770817122'})
+        print(response.text)
+
+    @classmethod
+    def update_info(cls):
+        payload = {
+            'password': f'New{fake.unix_time()}',
+            'phone_number': f'New{fake.phone_number()}',
+        }
+        response = requests.put(url=cls._URL, json=payload, params={'account': '993392921'})
+        print(response.text)
